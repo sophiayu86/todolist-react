76d524a973f37e549aa6dd6ac4e4c4979597800e
sophiayu86 <sophiayu86@gmail.com>
2023-03-08 21:39:53 +0800

    Feat: AuthInput Prop

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -6,8 +6,13 @@ import {
 } from 'components/common/auth.styled';
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUpPage = () => {
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <AuthContainer>
       <div>
@@ -16,18 +21,35 @@ const SignUpPage = () => {
       <h1>建立您的帳號</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label={'帳號'}
+          value={userName}
+          placeholder={'請輸入帳號'}
+          onChange={(nameInputValue) => setUserName(nameInputValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label={'Email'}
+          value={email}
+          placeholder={'請輸入 email'}
+          onChange={(emailInputValue) => setEmail(emailInputValue)}
+        />
       </AuthInputContainer>
-
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="password"
+          label="密碼"
+          value={password}
+          placeholder="請輸入密碼"
+          onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+        />
       </AuthInputContainer>
       <AuthButton>註冊</AuthButton>
-      <AuthLinkText>取消</AuthLinkText>
+      <Link to="/login">
+        <AuthLinkText>取消</AuthLinkText>
+      </Link>
     </AuthContainer>
   );
 };
