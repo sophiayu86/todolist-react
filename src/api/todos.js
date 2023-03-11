a93596bb7101715e91294bd36473ee98444753c2
sophiayu86 <sophiayu86@gmail.com>
2023-03-12 00:51:04 +0800

    Feat: write deploy script

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,10 +1,74 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001';
+// const baseUrl = 'http://localhost:3001';
+// export const getTodos = async () => {
+//   try {
+//     const res = await axios.get(`${baseUrl}/todos`);
+//     return res.data;
+//   } catch (error) {
+//     console.error('[Get Todos failed]: ', error);
+//   }
+// };
+
+// export const createTodo = async (payload) => {
+//   const { title, isDone } = payload;
+//   try {
+//     const res = await axios.post(`${baseUrl}/todos`, {
+//       title,
+//       isDone,
+//     });
+//     return res.data;
+//   } catch (error) {
+//     console.error('[Create Todo failed]: ', error);
+//   }
+// };
+
+// export const patchTodo = async (payload) => {
+//   const { id, title, isDone } = payload;
+//   try {
+//     const res = await axios.patch(`${baseUrl}/todos/${id}`, {
+//       title,
+//       isDone,
+//     });
+//     return res.data;
+//   } catch (error) {
+//     console.error('[Patch Todo failed]:', error);
+//   }
+// };
+
+// export const deleteTodo = async (id) => {
+//   try {
+//     const res = await axios.delete(`${baseUrl}/todos/${id}`);
+//     return res.data;
+//   } catch (error) {
+//     console.error('[Delete Todo failed]:', error);
+//   }
+// };
+
+// change json server to Todo-list server
+const baseUrl = 'https://todo-list.alphacamp.io/api';
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.error(error);
+  },
+);
+
 export const getTodos = async () => {
   try {
-    const res = await axios.get(`${baseUrl}/todos`);
-    return res.data;
+    const res = await axiosInstance.get(`${baseUrl}/todos`);
+    return res.data.data;
   } catch (error) {
     console.error('[Get Todos failed]: ', error);
   }
@@ -12,8 +76,9 @@ export const getTodos = async () => {
 
 export const createTodo = async (payload) => {
   const { title, isDone } = payload;
+
   try {
-    const res = await axios.post(`${baseUrl}/todos`, {
+    const res = await axiosInstance.post(`${baseUrl}/todos`, {
       title,
       isDone,
     });
@@ -26,10 +91,11 @@ export const createTodo = async (payload) => {
 export const patchTodo = async (payload) => {
   const { id, title, isDone } = payload;
   try {
-    const res = await axios.patch(`${baseUrl}/todos/${id}`, {
+    const res = await axiosInstance.patch(`${baseUrl}/todos/${id}`, {
       title,
       isDone,
     });
+
     return res.data;
   } catch (error) {
     console.error('[Patch Todo failed]:', error);
@@ -38,7 +104,7 @@ export const patchTodo = async (payload) => {
 
 export const deleteTodo = async (id) => {
   try {
-    const res = await axios.delete(`${baseUrl}/todos/${id}`);
+    const res = await axiosInstance.delete(`${baseUrl}/todos/${id}`);
     return res.data;
   } catch (error) {
     console.error('[Delete Todo failed]:', error);
